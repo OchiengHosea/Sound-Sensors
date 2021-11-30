e9abcdf19398592c5d912a023e8a21caec0b6c43
OchiengHosea <jrhosea8@gmail.com>
2021-11-30 20:45:09 +0300

    Small Sound Sensor

---
+++ index.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+
+#GPIO SETUP
+sound = 17
+led = 27
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(sound, GPIO.IN)
+# GPIO.setup(led,GPIO.OUT)
+def callback(sound):
+        if GPIO.input(sound):
+            print("Sound Detected!")
+            # GPIO.output(led,HIGH)
+        else:
+            print("No sound Detected!")
+		    # GPIO.output(led,LOW)
+      
+
+GPIO.add_event_detect(sound, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_callback(sound, callback)  # assign function to GPIO PIN, Run function on change
+
+while True:
+    time.sleep(1)
